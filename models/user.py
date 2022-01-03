b36345d50d962a606c5d9e8bb1c05e4c47bfcf7c
imerla1 <giorgi1048@yahoo.com>
2022-01-03 18:28:26 +0400

    Created user model

--- models/user.py
+++ models/user.py
@@ -1,1 +1,10 @@
 # User model will be declared here.
+from enum import unique
+from app import db
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String) # You should store password hash instead of raw password, for testing.
